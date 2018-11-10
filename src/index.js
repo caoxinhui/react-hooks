5958db97bcf8396e962c1d524168c2537aea45bb
cxh曹昕卉 <caoxh@ctrip.com>
2018-11-10 14:26:18 +0800

    feat: 添加特效页

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import I18nContext from './I18nContext'
+import data from './data'
+import './card.css'
+import CustomHookCard from './CostomHook/Card'
+import { useToggle, useI18n, useHash } from './CostomHook/hooks'
+import ClassCard from './Class/Card'
+import HookCard from './Hook/Card'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Components = {
+	class: ClassCard,
+	hook: HookCard,
+	custom: CustomHookCard
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const App = ({ data }) => {
+	let style = useHash()
+	let TargetCard = Components[style] || CustomHookCard
+	let [locale, toggleLocale] = useToggle(...data.locales)
+	console.log('style', style)
+	return (
+		<React.Fragment>
+			<I18nContext.Provider value={locale}>
+				<Local data={data} toggle={toggleLocale} />
+				<TargetCard data={data} />
+			</I18nContext.Provider>
+		</React.Fragment>
+	)
+}
+
+const Local = ({ data, toggle }) => {
+	let { locale } = useI18n(data, I18nContext)
+	return (
+		<div className="twitter__link" onClick={toggle}>
+			{locale}
+		</div>
+	)
+}
+
+
+ReactDOM.render(<App data={data}/>, document.getElementById('root'));
 serviceWorker.unregister();
